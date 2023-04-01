45c906549e8c6214013a774ea20e01c6be626a86
Andrea Lopez <andrea.lopez1114@stu.mtec.edu>
2023-03-31 18:24:22 -0600

    Week5Day4

---
+++ Week5Day4Project/app.js
@@ -0,0 +1,10 @@
+let navItem= $('.navItem').css('color','white');
+
+let nav= $('#nav').css('background-color','#145');
+
+let evenList= $('li:even').css('background-color','#ccc');
+
+let oddList= $('li:odd').css('background-color','#aaa');
+
+let input= $('#myInput').val('Andrea Lopez');
+
